07023dd01e70f1ee663f9ff3d86d0894e010c04f
chingkamhing <kam_ching@hotmail.com>
2021-02-21 12:34:51 +0800

    - seperate api and helloworld grpc server - move 2 main.go files under directory cmd - update Makefile

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,42 +3,15 @@ package main
 import (
 	"context"
 	"log"
-	"net"
 	"net/http"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 
 	apipb "helloworld-grpc-gateway/proto/api"
-	helloworldpb "helloworld-grpc-gateway/proto/helloworld"
 )
 
-type server struct {
-	// Embed the unimplemented server
-	helloworldpb.UnimplementedGreeterServer
-}
-
-func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
-	return &helloworldpb.HelloReply{Message: "Hello " + in.Name}, nil
-}
-
 func main() {
-	// Create a listener on TCP port
-	lis, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalln("Failed to listen:", err)
-	}
-
-	// Create a gRPC server object
-	s := grpc.NewServer()
-	// Attach the Greeter service to the server
-	helloworldpb.RegisterGreeterServer(s, &server{})
-	// Serve gRPC server
-	log.Println("Serving gRPC on 0.0.0.0:8080")
-	go func() {
-		log.Fatalln(s.Serve(lis))
-	}()
-
 	// Create a client connection to the gRPC server we just started
 	// This is where the gRPC-Gateway proxies the requests
 	conn, err := grpc.DialContext(
